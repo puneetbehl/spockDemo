9bb6d7c927bbcac3be85556346f6b8a5ed03cee4
Puneet <puneet.behl@intelligrape.com>
2014-07-15 22:52:45 +0530

    Implemented MIME type handling example for XML.

---
+++ grails-app/controllers/com/ig/spockdemo/SampleController.groovy
@@ -0,0 +1,13 @@
+package com.ig.spockdemo
+
+class SampleController {
+
+    def index() {
+        def data = ["Hello": "World"]
+        request.withFormat {
+            xml {render data as grails.converters.XML}
+            json {render data as grails.converters.JSON}
+        }
+
+    }
+}
---
+++ test/unit/com/ig/spockdemo/SampleControllerSpec.groovy
@@ -0,0 +1,26 @@
+package com.ig.spockdemo
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(SampleController)
+class SampleControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "SampleController: MIME type handling example"() {
+        when:
+        request.contentType = 'application/xml'
+        controller.index()
+
+        then:
+        response.text == '<xml version="1.0" encoding="UFT-8"?><map><entry key="Hello">World</entry></map>'
+    }
+}
